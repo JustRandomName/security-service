883606a5282f515e71bc807f9490e656c6981a33
Nikita Zhuchkevich <n.zhuchkevich@itransition.com>
2020-09-21 18:51:20 +0300

    Set up environment

--- src/main/java/security/SecurityService.java
+++ src/main/java/security/SecurityService.java
@@ -6,11 +6,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableZuulProxy
-@EntityScan(basePackages = {"model.entity"})
+@ComponentScan(basePackages = "model.entity")
 public class SecurityService {
     public static void main(String[] args) {
         run(SecurityService.class, args);
